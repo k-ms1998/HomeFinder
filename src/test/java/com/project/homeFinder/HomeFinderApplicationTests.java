b91793efbf5fd2346b2d2609350a741edef0aaca
Min Seop Kim <k_ms1998@naver.com>
2022-12-29 14:16:38 +0900

    Init Spring

---
+++ src/test/java/com/project/homeFinder/HomeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.homeFinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
