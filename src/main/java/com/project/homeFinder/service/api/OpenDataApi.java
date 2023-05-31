8e2602e3ab98ec19c45cc8e5bf68deafbe288f54
Min Seop Kim <k_ms1998@naver.com>
2023-05-31 17:15:59 +0900

    Add #22: 법정동 단위로 아파트 정보 가져오기

--- src/main/java/com/project/homeFinder/service/api/OpenDataApi.java
+++ src/main/java/com/project/homeFinder/service/api/OpenDataApi.java
@@ -42,4 +42,23 @@ public class OpenDataApi {
         return openDataFindAllApt(1, 10);
     }
 
+    // 국토교통부_법정동 아파트 목록
+    public ApartmentBasicInfoXmlResponseRaw openDataFindAllAptBjdCode(int page, int size, String bjdCode) {
+        final String uri =
+                String.format("https://apis.data.go.kr/1613000/AptListService2/getLegaldongAptList?serviceKey=%s&pageNo=%d&numOfRows=%d&bjdCode=%s",
+                        OPEN_DATA_API_KEY, page, size, bjdCode);
+
+        try {
+            return webClient.get()
+                    .uri(new URI(uri)) // 공공데이터는 URI 를 String 그대로 요청을 보내면 'SERVICE_KEY_IS_NOT_REGISTERED_ERROR' 오류 발생 -> URI 객체로 생성해서 요청 보내기
+                    .accept(MediaType.APPLICATION_XML)
+                    .retrieve()
+                    .bodyToMono(ApartmentBasicInfoXmlResponseRaw.class)
+                    .single().blockOptional()
+                    .orElse(new ApartmentBasicInfoXmlResponseRaw(null, null));
+
+        } catch (Exception e) {
+            throw new RuntimeException("[OpenDataApi]->{openDataFindAllApt} | Error. Could not fetch Open Data.");
+        }
+    }
 }
