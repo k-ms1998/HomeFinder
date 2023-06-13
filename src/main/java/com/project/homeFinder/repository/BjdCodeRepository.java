e569543ab4beb84b8a61fd1724bafb37f4b707a1
Min Seop Kim <k_ms1998@naver.com>
2023-06-13 16:33:34 +0900

    Add: 시 코드로 검색하기 + 페이징

--- src/main/java/com/project/homeFinder/repository/BjdCodeRepository.java
+++ src/main/java/com/project/homeFinder/repository/BjdCodeRepository.java
@@ -1,10 +1,13 @@
 package com.project.homeFinder.repository;
 
 import com.project.homeFinder.domain.BjdCode;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BjdCodeRepository extends JpaRepository<BjdCode, Long> {
 
     Long countByRegionCode(String regionCode);
 
+    Page<BjdCode> findAllByHighCode(String highCode, PageRequest pageRequest);
 }
