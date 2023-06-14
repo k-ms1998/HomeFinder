5ed0a10d7f11681967fbc94a1b1bed0c2ceca9ed
Min Seop Kim <k_ms1998@naver.com>
2023-06-14 23:41:43 +0900

    Add #21: 모든 법정동 코드 받아오기 -> 각 법정동에 있는 아파트 정보 받아와서 저장하기

--- src/main/java/com/project/homeFinder/batch/job/step/reader/OpenDataApiApartmentBasicInfoReader.java
+++ src/main/java/com/project/homeFinder/batch/job/step/reader/OpenDataApiApartmentBasicInfoReader.java
@@ -4,7 +4,10 @@ import com.project.homeFinder.dto.response.raw.xml.ApartmentListResponseRaw;
 import com.project.homeFinder.service.api.OpenDataApi;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.batch.item.*;
+import org.springframework.batch.item.ItemReader;
+import org.springframework.batch.item.NonTransientResourceException;
+import org.springframework.batch.item.ParseException;
+import org.springframework.batch.item.UnexpectedInputException;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -23,7 +26,11 @@ public class OpenDataApiApartmentBasicInfoReader implements ItemReader<Apartment
      */
     @Override
     public ApartmentListResponseRaw read() throws Exception, UnexpectedInputException, ParseException, NonTransientResourceException {
-        if (IS_END) {
+        if (IS_END) { // Read가 끝나는 시점
+            IS_END = false; // Read가 끝나는 시점에서, 다시 IS_END를 초기화 시켜줘야, 다음 법정동 코드로 Read 할때 바로 종료가 안됨 
+                            // -> 현재 Read가 끝나면, apartmentBasicInfoXmlResponseRawItemReader에서 OpenDataApiApartmentBasicInfoReader 객체를 새로 만들지 않고, 처음 호출 됐을때 생성된 객체를 다시 사용
+                            //  ->파라미터 값들이 계속 유지됨 -> 그러므로, IS_END랑 page 값들을 초기화 시켜줘야 함
+            page = 1;
             return null;
         }
 
@@ -34,8 +41,8 @@ public class OpenDataApiApartmentBasicInfoReader implements ItemReader<Apartment
             data = openDataApi.openDataFindAllAptBjdCode(page, size, BJD_CODE);
         }
 
-        log.info("[OpenDataApiApartmentBasicInfoReader] currentPage={}", page);
-        if (data.fetchItems().size() == 0 || page > 5) {
+        log.info("[OpenDataApiApartmentBasicInfoReader] BJD_CODE={}, currentPage={}", BJD_CODE, page);
+        if (data.fetchItems().size() == 0) {
             return null;
         }
         if(data.fetchItems().size() < size){
