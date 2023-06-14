33b76607763a2af62942e6e72f8c20d233c1ae10
Min Seop Kim <k_ms1998@naver.com>
2023-06-14 19:55:26 +0900

    Add: 시 코드로 법정동 코드들 반환하기

--- src/main/java/com/project/homeFinder/repository/BjdCodeRepository.java
+++ src/main/java/com/project/homeFinder/repository/BjdCodeRepository.java
@@ -4,10 +4,17 @@ import com.project.homeFinder.domain.BjdCode;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BjdCodeRepository extends JpaRepository<BjdCode, Long> {
 
     Long countByRegionCode(String regionCode);
 
     Page<BjdCode> findAllByHighCode(String highCode, PageRequest pageRequest);
+
+    @Query("SELECT b.regionCode FROM BjdCode b WHERE b.sidoCode=:sidoCode")
+    List<String> findAllBySidoCode(String sidoCode);
+
 }
